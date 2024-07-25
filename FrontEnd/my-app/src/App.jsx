9e91c7948269a29c323ade8c107485b777cb0b7a
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-07-25 21:20:34 +0530

    adding-homepage-navbar

--- FrontEnd/my-app/src/App.jsx
+++ FrontEnd/my-app/src/App.jsx
@@ -2,15 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter,Route,Router, Routes } from 'react-router-dom'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import HomePage from './Pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
+      <div className='page_container'>
+
+        <BrowserRouter>
+       <Routes>
+
+      <Route  exact path='/' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/home' element={<HomePage/>}/>
       
-      
-      
+   
+
+       </Routes>
+
+
+      </BrowserRouter>
+
+      </div>
         </>
   )
 }
---
+++ FrontEnd/my-app/src/Pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function HomePage() {
+  return (
+    <>
+    <Navbar>
+      <div className='home_page_container'>
+
+
+
+      </div>
+
+        
+    </Navbar>
+
+    </>
+  )
+}
+
+export default HomePage
